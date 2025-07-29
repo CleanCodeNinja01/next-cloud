8bb6dad527067184387b766939f89f1d385903b2
CleanCodeNinja01 <saleha.shujaat1@gmail.com>
2025-07-30 00:29:12 +0500

    Removed node modules and project is running

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div className="flex flex-row justify-between items-center">
       <span>Home Page</span>
       <span className="flex flex-row items-end space-x-4">
-        <Link href={"/performance"}>Goto Perf Page! </Link>
-        <Link href={"/reliability"}>Goto Rel Page! </Link>
-        <Link href={"/scale"}>Goto Scale Page! </Link>
+        <Link href="/performance">Goto Perf Page!</Link>
+        <Link href="/reliability">Goto Rel Page!</Link>
+        <Link href="/scale">Goto Scale Page!</Link>
       </span>
     </div>
   );
